482f85475b8b01540b486684c075e1f28fc22c5c
Tomi M <tomitm@gmail.com>
2015-11-09 16:29:47 -0800

    Initial version - 1.0.0.

---
+++ README.md
@@ -0,0 +1,7 @@
+# List Enhancements for Twitter
+
+Twitter's lists are incredibly helpful for managing the noise-to-signal ratio
+in your feed, however, Twitter has them hidden away under the user menu in the header.
+
+This extension brings them out right besides your main timeline and improves the
+usability of the list Twitter shows when you're viewing a list as well.
---
+++ app/contentscript.js
@@ -0,0 +1,97 @@
+'use strict';
+
+function getUsername() {
+  // username is conveniently available as a data property on an element
+  var user = document.getElementsByClassName('js-mini-current-user')[0];
+  if(!user) {
+    return;
+  }
+  return user.dataset.screenName;
+}
+
+function fetchLists(username) {
+  if (!username) {
+    return Promise.reject("No username.");
+  }
+
+  // we're part of the page, so do as Twitter does.
+  // downside is their cookies only have template access, so no clean API,
+  // but it allows this can work out of the box since OAuth isn't needed.
+  var options = {
+    credentials: 'include',
+    headers: {
+      accept: 'application/json, text/javascript, */*; q=0.01',
+      'x-push-state-request': true // required to return template
+    }
+  };
+  return fetch(`/${username}/lists`, options)
+    .then(res => res.json());
+}
+
+function extractLists(res) {
+  if(!res.page) {
+    return Promise.reject("Invalid response received.");
+  }
+  // response seems to be the whole page, but we only really need
+  // the ProfileListItem-name <a> elements.
+  var page = document.createElement('div');
+  page.innerHTML = res.page;
+  return page.getElementsByClassName('ProfileListItem-name');
+}
+
+function createListsModule(links) {
+  // make it feel like part of the dashboard...
+  var linkList = [...links]
+                    .map(link => `<li>${link.outerHTML}</li>`)
+                    .join('');
+
+  return `<div class="lists-inner">
+            <div class="flex-module">
+              <div class="flex-module-header">
+                <h3>Lists</h3>
+              </div>
+              <div class="flex-module-inner">
+                <ul class="list-of-lists">${linkList}</ul>
+              </div>
+            </div>
+          </div>`;
+}
+
+function addToPage(html) {
+  var dashboard = document.getElementsByClassName('dashboard-left')[0];
+  if (!dashboard) {
+    // bail if we can't find the dashboard; nothing to append to
+    return;
+  }
+  var listsElement = document.createElement('div');
+  listsElement.className = 'Lists module';
+  dashboard.appendChild(listsElement);
+  listsElement.innerHTML = html;
+}
+
+function setup() {
+  var listOfLists = document.getElementsByClassName('list-of-lists')[0];
+  if (!!listOfLists) {
+    // bail if there's already lists on the page
+    return;
+  }
+
+  var username = getUsername();
+  fetchLists(username)
+    .then(extractLists)
+    .then(createListsModule)
+    .then(addToPage)
+    .catch((err) => { console.debug("[lists]", err); });
+}
+
+// check for path change
+var lastpath = window.location.pathname;
+setInterval(function() {
+  if (window.location.pathname === lastpath) {
+    return;
+  }
+  lastpath = window.location.pathname;
+  setup();
+}, 500);
+
+window.addEventListener("load", setup);
